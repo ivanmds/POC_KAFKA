12fd713e92ab8ad1adc5cc07c9457f2d6742ebd8
Ivan Meireles Da Silva <isilva@acesso.com>
2020-09-17 23:36:15 -0300

    ajustes

---
+++ PocKafka/PocKafka/Consumers/Worker.cs
@@ -0,0 +1,58 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PocKafka.Consumers
+{
+    public class Worker : BackgroundService
+    {
+        private IConsumer<Ignore, string> consumer;
+        private readonly IServiceProvider services;
+
+        public Worker(IServiceProvider services)
+        {
+            this.services = services;
+        }
+
+        public override async Task StartAsync(CancellationToken cancellationToken)
+        {
+            var conf = new ConsumerConfig
+            {
+                GroupId = "test-consumer-group",
+                BootstrapServers = "localhost:9092",
+                // Note: The AutoOffsetReset property determines the start offset in the event
+                // there are not yet any committed offsets for the consumer group for the
+                // topic/partitions of interest. By default, offsets are committed
+                // automatically, so in this example, consumption will only start from the
+                // earliest message in the topic 'my-topic' the first time you run the program.
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            consumer = new ConsumerBuilder<Ignore, string>(conf).Build();
+            consumer.Subscribe("test-topic");
+            await base.StartAsync(cancellationToken);
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            using var scope = services.CreateScope();
+            await Task.Run(() =>
+            {
+                while (!stoppingToken.IsCancellationRequested)
+                {
+                    var cr = consumer.Consume(stoppingToken);
+                    Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+                }
+            });
+        }
+
+        public override void Dispose()
+        {
+            consumer.Close();
+            base.Dispose();
+        }
+    }
+}
---
+++ PocKafka/PocKafka/Controllers/Test.cs
@@ -0,0 +1,35 @@
+﻿using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace PocKafka.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Test : ControllerBase
+    {
+        public async Task<IActionResult> Get()
+        {
+            var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+            // If serializers are not specified, default serializers from
+            // `Confluent.Kafka.Serializers` will be automatically used where
+            // available. Note: by default strings are encoded as UTF8.
+            using (var p = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    var dr = await p.ProduceAsync("test-topic", new Message<Null, string> { Value = "test" });
+                    Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
+            }
+            return Ok();
+        }
+    }
+}
